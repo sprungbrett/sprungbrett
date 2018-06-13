83083916235b255a61a465098f4db815caffe7bd
Johannes Wachter <johannes.wachter@sulu.io>
2018-06-13 22:45:01 +0200

    added title and uuid to course

--- src/Component/Course/Model/Course.php
+++ src/Component/Course/Model/Course.php
@@ -2,6 +2,34 @@
 
 namespace Sprungbrett\Component\Course\Model;
 
+use Sprungbrett\Component\Uuid\Model\Uuid;
+use Sprungbrett\Component\Uuid\UuidTrait;
+
 class Course
 {
+    use UuidTrait;
+
+    /**
+     * @var string
+     */
+    private $title;
+
+    public function __construct(string $title, ?Uuid $uuid = null)
+    {
+        $this->initializeUuid($uuid);
+
+        $this->title = $title;
+    }
+
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
 }
